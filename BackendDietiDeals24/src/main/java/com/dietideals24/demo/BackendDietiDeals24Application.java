655472e387f1112c3ad23695b5c05b1729e973c2
Loris Zannini <loriszannini.74@gmail.com>
2023-12-04 18:39:54 +0100

    Inizializzazione backend spring boot

---
+++ BackendDietiDeals24/src/main/java/com/dietideals24/demo/BackendDietiDeals24Application.java
@@ -0,0 +1,13 @@
+package com.dietideals24.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDietiDeals24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDietiDeals24Application.class, args);
+	}
+
+}
---
+++ BackendDietiDeals24/src/test/java/com/dietideals24/demo/BackendDietiDeals24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dietideals24.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDietiDeals24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
